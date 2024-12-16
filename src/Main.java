e88feadfa5c579a51b14e31848f62662451560ff
rikuto kuniyoshi <e245758@ie.u-ryukyu.ac.jp>
2024-12-17 02:54:00 +0900

    step3

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,6 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        String str = null;
-
-        try {
-            System.out.println((str.length()));
-        } catch (NullPointerException e) {
-            System.out.println("エラー〜");
-            System.out.println("エラー" + e.getMessage());
-        }
+        String str = "壱百満";
+        int value = Integer.parseInt(str);
     }
 }
