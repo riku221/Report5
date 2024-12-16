2a5596b0f51d79d80fffbb173797e5a22230523e
rikuto kuniyoshi <e245758@ie.u-ryukyu.ac.jp>
2024-12-17 02:51:48 +0900

    step2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) throws Exception {
         String str = null;
-        System.out.println(str.length());
+
+        try {
+            System.out.println((str.length()));
+        } catch (NullPointerException e) {
+            System.out.println("エラー〜");
+            System.out.println("エラー" + e.getMessage());
+        }
     }
 }
